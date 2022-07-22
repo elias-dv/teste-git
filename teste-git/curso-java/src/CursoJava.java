3310d4029d42aad038c677c85250252fbc555003
elias <elias@air-de-elias>
2022-07-22 11:27:23 -0300

    comitanddo

--- teste-git/curso-java/src/CursoJava.java
+++ teste-git/curso-java/src/CursoJava.java
@@ -2,7 +2,10 @@ class CursoJava {
 
     public static void main(String[] args) {
    
-       System.out.println("Primeiro progama em Java");
+       System.out.println("Primeiro em progama em Java");
+       
+       System.out.println("segundo em progama em Java");
+
 
    }
 
